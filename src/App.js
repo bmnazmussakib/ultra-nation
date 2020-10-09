c7daae9229a31f6f5bdb8ad2c666cd347b86ba30
bmnazmussakib <nazmussakib161@gmail.com>
2020-10-10 02:43:19 +0600

    half done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [countries, setCountries] = useState([]);
+
+  useEffect(function countries() {
+    fetch('https://restcountries.eu/rest/v2/all')
+      .then(res => res.json())
+      .then(data => {
+        setCountries(data)
+        const names = data.map(country => country.name)
+        console.log(names)
+      })
+      .catch(error => console.log(error));
+
+  }, [])
+
+
+
+
+
   return (
     <div className="App">
       <header className="App-header">
+
+        <h1>Number of country : {}</h1>
+        <button >Click</button>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
